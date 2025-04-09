bb9f4a5080edcb2ecf85a821de32deb8c5f90549
Yamamaamirr <myamama.be21igis@student.nust.edu.pk>
2025-04-10 03:14:35 +0500

    check

--- components/Vehicle Table/ui/sidebar.tsx
+++ components/Vehicle Table/ui/sidebar.tsx
@@ -7,17 +7,17 @@ import { PanelLeft } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent } from "@/components/ui/sheet"
-import { Skeleton } from "@/components/ui/skeleton"
+import { Button } from "@/components/Vehicle Table/ui/button"
+import { Input } from "@/components/Vehicle Table/ui/input"
+import { Separator } from "@/components/Vehicle Table/ui/separator"
+import { Sheet, SheetContent } from "@/components/Vehicle Table/ui/sheet"
+import { Skeleton } from "@/components/Vehicle Table/ui/skeleton"
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
+} from "@/components/Vehicle Table/ui/tooltip"
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
